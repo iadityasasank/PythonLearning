889728154ff3c587ebf041c38b7a7584d9b58339
iadityasasank <50364371+iadityasasank@users.noreply.github.com>
2023-12-27 11:55:40 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,6 @@
-print("Hello Replit")
+print("Aditya")
+print("Decmeber 27, 2023")
+print("I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.)
+print("I am feeling 🥳")
+print("You can follow my progress at https://github.com/iadityasasank/PythonLearning")
